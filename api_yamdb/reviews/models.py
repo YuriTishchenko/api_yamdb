b929f492d3a681714d4bf45a2f8c1ec9676b0cb3
Ivan <voitsik.ivan@yadex.ru>
2023-06-13 23:42:14 +0300

    Начало работы с моделями

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genres
+    )
+    category = models.ForeignKey(
+        Categories
+    )
+
+
+class Reviews(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
