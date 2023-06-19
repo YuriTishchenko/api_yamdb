fdf400cab274966f5f858b64b9f76935cdf793d6
Ivan <voitsik.ivan@yadex.ru>
2023-06-19 17:38:34 +0300

    Create TitleSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,11 +8,29 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Categories
         exclude = ('id',)
-        lookup_field = 'slug'
+        lookup_field = 'name'
+
 
 class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Genres
         exclude = ('id',)
-        lookup_field = 'slug'
+        lookup_field = 'name'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genere = serializers.SlugRelatedField(
+        slug_field='slug',
+        many=True,
+        queryset=Genres.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Categories.objects.all()
+    )
+
+    class Meta:
+        model = Titles
+        fields = '__all__'
+
